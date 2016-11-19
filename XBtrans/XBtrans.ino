bd0b7ab8a8eb5fc43ffe187e19a023093e1803e3
Matt B <boubinmj@miamioh.edu>
2016-11-19 18:22:27 -0500

    added xbee basic communication files

---
+++ XBtrans/XBtrans.ino
@@ -0,0 +1,58 @@
+/*****************************************************************
+XBee_Serial_Passthrough.ino
+
+Set up a software serial port to pass data between an XBee Shield
+and the serial monitor.
+
+Hardware Hookup:
+  The XBee Shield makes all of the connections you'll need
+  between Arduino and XBee. If you have the shield make
+  sure the SWITCH IS IN THE "DLINE" POSITION. That will connect
+  the XBee's DOUT and DIN pins to Arduino pins 2 and 3.
+
+*****************************************************************/
+// We'll use SoftwareSerial to communicate with the XBee:
+#include <SoftwareSerial.h>
+// XBee's DOUT (TX) is connected to pin 2 (Arduino's Software RX)
+// XBee's DIN (RX) is connected to pin 3 (Arduino's Software TX)
+SoftwareSerial XBee(2, 3); // RX, TX
+
+  int pingpong = 1;
+  byte b0 = 0b00000001;
+  
+void setup()
+{
+  // Set up both ports at 9600 baud. This value is most important
+  // for the XBee. Make sure the baud rate matches the config
+  // setting of your XBee.
+  XBee.begin(9600);
+  Serial.begin(9600);
+
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+
+  XBee.write(b0);
+
+  if(pingpong == 0)
+    digitalWrite(13, LOW);
+  else
+    digitalWrite(13,HIGH);
+
+  pingpong = 1 - pingpong;
+
+  delay(1000);
+  
+  /*
+  if (Serial.available())
+  { // If data comes in from serial monitor, send it out to XBee
+    XBee.write(Serial.read());
+  }
+  if (XBee.available())
+  { // If data comes in from XBee, send it out to serial monitor
+    Serial.write(XBee.read());
+  }
+  */
+}
