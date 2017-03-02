d9797cefd43d328d8a8ba051245955ac34990eee
benjamam <benjamam@miamioh.edu>
2017-03-02 11:16:54 -0500

    fixed charts on mobile

--- Website/charts.js
+++ Website/charts.js
@@ -7,22 +7,22 @@ var myChart = new Chart(ctx, {
         datasets: [{
             label: 'Average Temperature over Past 3 Months',
             data: [22, 31, 42],
-            backgroundColor: [
-                'rgba(0, 0, 255, 0.3)',
+            backgroundColor: 'rgba(0, 0, 255, 0.3)' , /*[
+                'rgba(0, 0, 255, 0.3)' ,
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
+                'rgba(255, 159, 64, 0.2)' 
+            ],*/
+            borderColor: 'rgba(255,99,132,1)', /*[
+                'rgba(255,99,132,1)' ,
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+                'rgba(255, 159, 64, 1)' 
+            ],*/
             borderWidth: 1
         }]
     },
@@ -47,22 +47,22 @@ var myChart = new Chart(ctx, {
         datasets: [{
             label: 'Temperature over Last 12 Hours',
             data: populateHourTemps(),
-            backgroundColor: [
+            backgroundColor: 'rgba(0, 255, 0, 0.3)',/*[
                 'rgba(0, 255, 0, 0.3)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
+                'rgba(255, 159, 64, 0.2)' 
+            ],*/
+            borderColor: 'rgba(255,99,132,1)',/*[
                 'rgba(255,99,132,1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+                'rgba(255, 159, 64, 1)' 
+            ],*/
             borderWidth: 1
         }]
     },
