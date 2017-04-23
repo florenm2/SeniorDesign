7c35478f28fafd58ad6fdd1eb588eceefa108d1d
Matthew Boubin <boubinmj@miamioh.edu>
2017-04-23 18:55:57 +0000

    added receiver code from RPI

---
+++ Receiver/receiver_node.py
@@ -0,0 +1,36 @@
+import serial
+import sys
+import requests
+import json
+from time import gmtime, strftime
+
+dateTime = strftime("%Y/%m/%d %H:%M:%S", gmtime())
+
+def ASCII(s):
+	x = 0
+	for i in xrange(len(s)):
+		#sys.stdout.write("byte: ")	
+		print "byte ",i, ":", ord(s[i])
+	return
+
+serial = serial.Serial('/dev/ttyAMA0', 9600)
+packet = serial.read(50)
+ASCII(packet)
+
+temp = ord(packet[10]) 
+inches = 4.0 - ord(packet[34])/10.0
+moisture_perc = ord(packet[22])
+
+print "debug ", 0.0 + ord(packet[34])/10.0
+
+payload = json.dumps({'dateTime': dateTime , 'temperature' : temp, 'waterLevel' : inches, 'soilMoisture' : moisture_perc})
+
+header = {'Content-Type' : 'application/json'}
+uri = "https://api.mlab.com/api/1/databases/seniordesign/collections/metrics?apiKey=yYhdTVjbQWk0303jla3N_39DJp8RAJ4P"
+r = requests.post(uri, data=payload, headers=header)
+sys.stdout.write("request status: ")
+print (r.status_code)
+
+sys.stdout.write("get status: ")
+g = requests.get(uri, headers=header)
+print(g.status_code)
